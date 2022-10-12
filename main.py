db3a99f3491107d0859ad2b3a9b8204eb86be652
Patryk <115358483+Coffee-Whatever@users.noreply.github.com>
2022-10-12 22:41:32 +0200

    Quick fix for arrow rotation -//- Szybka poprawka obrotu strzałki
    Less responsive but the x and y axis doesn't flip when the arrow is set to anything but 0 degrees (most viable on 90 degrees).
    Could not get the arrow out of the corner without it flipping axis around.
    Mniej responsywny, ale oś x i y nie odwraca się, gdy strzałka jest ustawiona na wartość inną niż 0 stopni (najbardziej widoczna przy 90 stopniach)
    Niestety nie udało się wyciągnąć obrazu z rogu bez odwracania się osi.

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import kivy
+import sys
+import socket
+
 from kivy.app import App
-from kivy.app import runTouchApp
-from kivy.core.window import Window
 from kivy.clock import Clock
+from kivy.app import runTouchApp
 from kivy.uix.label import Label
 from kivy.uix.image import Image
+from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.core.window import Window
 from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics.context_instructions import PushMatrix, PopMatrix, Rotate
-import socket
-import sys
 
 kivy.require('2.1.0')
 
-x, y = 800, 800
-# x, y = Window.size
-Window.size = (x, y)
+x, y = Window.size
+if x < y:
+	x, y = 800, 800
+	Window.size = (x, y)
+
 Window.top = 35
 Window.left = 5
+
+class Obraz(Widget):
+	def __init__(self):
+		super().__init__()
+		self.Im = Image(source="arrow.png")
+		self.Im.size_hint = (1, 1)
+		self.size_hint = (0.1, 0.15)
+		self.pos_hint = {x: 1, y: 1}
+		self.add_widget(self.Im)
+
 class Main(FloatLayout):
 	def __init__(self, **kwargs):
 		super(Main, self).__init__(**kwargs)
@@ -31,7 +45,7 @@ class Main(FloatLayout):
 		self.L = Button(text="W lewo", size_hint=(0.2, 0.1))
 		self.R = Button(text="W prawo", size_hint=(0.2, 0.1))
 		self.T = Label(text="0.0", size_hint=(0.2, 0.1))
-		self.Im = Image(source="arrow.png")
+		self.Obr = Obraz()
 
 		self.L.bind(on_press=self.press)
 		self.R.bind(on_press=self.press)
@@ -41,7 +55,7 @@ class Main(FloatLayout):
 		self.add_widget(self.L)
 		self.add_widget(self.R)
 		self.add_widget(self.T)
-		self.add_widget(self.Im)
+		self.add_widget(self.Obr)
 
 		self.get_start_angle()
 		self.set_up_upload()
@@ -101,13 +115,13 @@ class Main(FloatLayout):
 		self.update(-0.1)
 		pass
 	def update(self, sign):
-		with self.Im.canvas.before:
+		with self.Obr.Im.canvas.before:
 			PushMatrix()
 			self.rot = Rotate()
 			self.rot.axis = (0, 0, 1)
-			self.rot.origin = self.center
+			self.rot.origin = self.Obr.Im.center
 			self.rot.angle = sign
-		with self.Im.canvas.after:
+		with self.Obr.Im.canvas.after:
 			PopMatrix()
 	def update_visuals(self, really):
 		global x, y
@@ -115,7 +129,6 @@ class Main(FloatLayout):
 		self.L.pos = (((1/3*x) - (0.5*0.2*x)), ((0.7*y) - (0.1*y*0.5)))
 		self.R.pos = (((2/3*x) - (0.5*0.2*x)), ((0.7*y) - (0.1*y*0.5)))
 		self.T.pos = (((0.5*x) - (0.5*0.2*x)), ((0.4*y) - (0.1*y*0.5)))
-		self.Im.pos = (0.0025*x, 0)
 
 class MyApp(App):
 	@staticmethod
